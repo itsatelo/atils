0c5b00a55f6819a174d36b16595673e709ead362
ItsAtelo <88721807+ItsAtelo@users.noreply.github.com>
2022-02-11 03:36:41 -0800

    Lol i need to fix this

--- @1.1.1/Utils/Errors/BuildError.js
+++
@@ -1,9 +0,0 @@
-const { ErrorBuilder } = require('../ErrorUtil.js');
-module.exports = class extends ErrorBuilder {
-    constructor(message) {
-        super('BuildError', message ?? 'Outdated build', {
-            date: true,
-            stackTrace: true,
-        });
-    }
-}
