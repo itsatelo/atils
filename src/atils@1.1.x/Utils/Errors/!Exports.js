120f43bb25daf6facc63ffa4715ba1baa25ea215
itsatelo <itsatelo@gmail.com>
2022-03-08 00:07:04 -0800

    @1.2.0 Release File Upload
    These are all the files for @1.2.0, as well as @1.1.4 (as you can backtrack to that version).
    It does NOT contain the files for the dev-build, though. I may add a branch for that.

---
+++ src/atils@1.1.x/Utils/Errors/!Exports.js
@@ -0,0 +1,5 @@
+module.exports = {
+    AtilsError: require('./AtilsError.js'),
+    BuildError: require('./BuildError.js'),
+    ConsoleError: require('./ConsoleError.js'),
+}
---
+++ src/atils@1.2.x/Enums/ConsoleStyles.js
@@ -0,0 +1,88 @@
+const Enum = require('../Utilities/Enum.js');
+
+// Mess with these in the future; goal: lower the count of the textStyle resolver
+// ConsoleStyles.textStyle.bright raises the brightness of the color, I haven't messed with the others.
+const ConsoleStyles = new Enum({
+    all: {
+        default: '\x1b[0m',
+        reset: '\x1b[0m',
+    },
+
+    textStyle: {
+        bright: '\x1b[1m',
+        dim: '\x1b[2m',
+        underscore: '\x1b[4m',
+        blink: '\x1b[5m',
+        reverse: '\x1b[7m',
+        invisible: '\x1b[8m',
+
+        dark: '\x1b[2m',
+        hidden: '\x1b[8m',
+
+        default: '\x1b[0m',
+        reset: '\x1b[0m',
+    },
+
+    textColor: {
+        black: '\x1b[30m',
+        red: '\x1b[31m',
+        green: '\x1b[32m',
+        yellow: '\x1b[33m',
+        blue: '\x1b[34m',
+        magenta: '\x1b[35m',
+        cyan: '\x1b[36m',
+        white: '\x1b[37m',
+
+        purple: '\x1b[35m',
+        light_blue: '\x1b[36m',
+
+        default: '\x1b[37m',
+        reset: '\x1b[37m',
+    },
+
+    backgroundColor: {
+        black: '\x1b[40m',
+        red: '\x1b[41m',
+        green: '\x1b[42m',
+        yellow: '\x1b[43m',
+        blue: '\x1b[44m',
+        magenta: '\x1b[45m',
+        cyan: '\x1b[46m',
+        white: '\x1b[47m',
+
+        purple: '\x1b[45m',
+        light_blue: '\x1b[46m',
+
+        default: '\x1b[47m',
+        reset: '\x1b[47m',
+    }
+});
+
+/**
+ * Resolves and returns provided Text Style.
+ * @param {string} style - The Text Style to be resolved. 
+ * @returns {string} - Console Text Style.
+ */
+function Style(style) {
+    return ConsoleStyles.resolveTextStyle(style);
+}
+
+/**
+ * Resolves and returns provided Text Color.
+ * @param {string} color - The Text Color to be resolved. 
+ * @returns {string} - Console Text Color.
+ */
+function Color(color) {
+    return ConsoleStyles.resolveTextColor(color);
+}
+
+/**
+ * Resolves and returns provided Background Color
+ * @param {string} color - The Background Color to be resolved. 
+ * @returns {string} - Console Background Color.
+ */
+function BackgroundColor(color) {
+    return ConsoleStyles.resolveBackgroundColor(color);
+}
+
+module.exports = { ConsoleStyles, Style, Color, BackgroundColor };
